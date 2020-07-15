ed324cedfd68825840c184efbbe8ec85b81e8c13
Md Tazbir Ur Rahman <tazbirs@gmail.com>
2020-07-15 22:00:05 +0600

    wrote simple Appjs and index js code for a basic rect app.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+
+const App=()=>(
+    <div className="App">
+        <h1>
+            Hello world!
+        </h1>
+    </div>
+);
+
+export default App;
+
+
